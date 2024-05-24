0ecf468f6c4924afb79aa977269a7842a9ab9836
ammara-Ilyas <ismarailyas545@gmail.com>
2024-05-24 23:50:48 +0500

    Responsive todo app

--- app/components/mainComponent/Aside.tsx
+++ app/components/mainComponent/Aside.tsx
@@ -5,7 +5,7 @@ import SearchFilter from "./SearchFilter";
 
 const Aside = () => {
   return (
-    <div className="w-[99%] md:w-3/4 h-[99%]  border-green-900 relative  bg-blue-500 rounded-2xl ml-auto mr-[3px]">
+    <div className="w-[99%] md:w-3/4 h-[99%] relative z-20 bg-blue-500 rounded-2xl ml-auto mr-[3px]">
       <Search />
       <SearchFilter />
       <div className="w-3/4 mx-auto h-[3px] bg-purple-500"></div>
--- app/components/mainComponent/SecMain.tsx
+++ app/components/mainComponent/SecMain.tsx
@@ -10,16 +10,16 @@ const SecMain = () => {
     setIsOpen((prev) => !prev);
   };
   return (
-    <div className="w-[95%] sm:w-[85%] md:w-[95%] lg:w-[85%] relative flex  justify-center h-full mx-auto sm:ml-auto overflow-hidden bg-gradient-to-t from-indigo-400 to-blue-500 ... ">
+    <div className="w-[95%] sm:w-[85%] md:w-[95%] lg:w-[85%] relative flex  justify-center h-full mx-auto sm:mr-16 sm:ml-auto overflow-hidden bg-gradient-to-t from-indigo-400 to-blue-500 ... ">
       <div
-        className=" text-2xl absolute  top-2 z-50 left-3 items-center"
+        className=" text-2xl absolute top-1 md:hidden block left-3 z-50 items-center"
         onClick={handleOpenNav}
       >
         <HiBars3 className="text-4xl text-white md:hidden block" />
       </div>
       <div className="w-[150px] h-[150px] rounded-full z-0 bg-yellow-400 absolute bottom-[40%] -right-16 "></div>
       <div className="w-[200px] h-[200px] z-40 bg-red-300 rounded-full absolute -bottom-14 -left-32 "></div>
-      <div className="w-[100px] h-[100px]  bg-cyan-400 rounded-full absolute top-0 left-56 lg:left-64 hidden md:block "></div>
+      <div className="w-[100px] h-[100px] z-30 bg-cyan-400 rounded-full absolute top-0 left-56 lg:left-64 md:block hidden"></div>
       <Sidebar isOpen={isOpen} />
     </div>
   );
